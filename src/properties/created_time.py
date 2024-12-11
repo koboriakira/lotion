d4c771396e3c68a46739b9d11c940f9d1ac22185
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-12-11 20:42:27 +0900

    refactor: 作成時刻と更新時刻のプロパティを使いやすいように修正

--- src/properties/created_time.py
+++ src/properties/created_time.py
@@ -1,14 +1,40 @@
-from datetime import date, datetime
+from datetime import datetime, timedelta
 
-from src.properties.date import Date
-from src.datetime_utils import convert_to_date_or_datetime
+from datetime_utils import JST
+from properties.property import Property
 
 
-class CreatedTime(Date):
-    NAME = "作成日時"
+class CreatedTime(Property):
+    value: datetime
 
-    @classmethod
-    def create(cls: "CreatedTime", value: str | date | datetime) -> "CreatedTime":
-        if isinstance(value, str):
-            value = convert_to_date_or_datetime(value)
-        return CreatedTime.from_start_date(name=cls.NAME, start_date=value)
+    def __init__(
+        self,
+        name: str,
+        value: datetime,
+        id: str | None = None,  # noqa: A002
+    ) -> None:
+        self.name = name
+        self.value = value
+        self.id = id
+
+    @staticmethod
+    def of(name: str, params: dict) -> "CreatedTime":
+        datetime_ = datetime.fromisoformat(params["created_time"]) + timedelta(hours=9)
+        datetime_ = datetime_.replace(tzinfo=JST)
+        return CreatedTime(name=name, value=datetime_, id=params.get("id"))
+
+    def __dict__(self) -> dict:
+        result = {
+            "type": self.type,
+            self.type: self.value.isoformat(),
+        }
+        if self.id is not None:
+            result["id"] = self.id
+        return result
+
+    def value_for_filter(self) -> str:
+        return self.value.isoformat()
+
+    @property
+    def type(self) -> str:
+        return "created_time"
--- src/properties/last_edited_time.py
+++ src/properties/last_edited_time.py
@@ -1,14 +1,42 @@
-from datetime import date, datetime
+from dataclasses import dataclass
+from datetime import datetime, timedelta
 
-from src.properties.date import Date
-from src.datetime_utils import convert_to_date_or_datetime
+from datetime_utils import JST
+from properties.property import Property
 
 
-class LastEditedTime(Date):
-    NAME = "最終更新日時"
+@dataclass
+class LastEditedTime(Property):
+    value: datetime
 
-    @classmethod
-    def create(cls, value: str | date | datetime) -> "LastEditedTime":
-        if isinstance(value, str):
-            value = convert_to_date_or_datetime(value)
-        return LastEditedTime.from_start_date(name=cls.NAME, start_date=value)
+    def __init__(
+        self,
+        name: str,
+        value: datetime,
+        id: str | None = None,  # noqa: A002
+    ) -> None:
+        self.name = name
+        self.value = value
+        self.id = id
+
+    @staticmethod
+    def of(name: str, params: dict) -> "LastEditedTime":
+        datetime_ = datetime.fromisoformat(params["last_edited_time"]) + timedelta(hours=9)
+        datetime_ = datetime_.replace(tzinfo=JST)
+        return LastEditedTime(name=name, value=datetime_, id=params.get("id"))
+
+    def __dict__(self) -> dict:
+        result = {
+            "type": self.type,
+            self.type: self.value.isoformat(),
+        }
+        if self.id is not None:
+            result["id"] = self.id
+        return result
+
+    def value_for_filter(self) -> str:
+        return self.value.isoformat()
+
+    @property
+    def type(self) -> str:
+        return "last_edited_time"
